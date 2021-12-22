75831189192587a02d1aecd291cbf0fff8b73910
Jaminur islam <jaminurislam250@gmail.com>
2021-12-22 19:17:36 +0600

    added sidebaar

--- src/component/Dashboard/Admin.js
+++ src/component/Dashboard/Admin.js
@@ -7,22 +7,15 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
-<<<<<<< HEAD
-import MenuIcon from "@mui/icons-material/Menu";
-=======
 import List from "@mui/material/List";
 import ListItemText from "@mui/material/ListItemText";
->>>>>>> b31c552849eb51de8bdeb45ac3dcb3de93294167
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import PropTypes from "prop-types";
 import * as React from "react";
 import { useStyle } from "../Style/Style";
-<<<<<<< HEAD
 import SidebarDrawer from "./SidebarDrawer";
-=======
 import Search from "./Search";
->>>>>>> b31c552849eb51de8bdeb45ac3dcb3de93294167
 
 const drawerWidth = 280;
 function ResponsiveDrawer(props) {
