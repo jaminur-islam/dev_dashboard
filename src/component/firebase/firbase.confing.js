8ad64dfd39ae2537f914a0ad9e41b60f0b53b57b
Jaminur islam <jaminurislam250@gmail.com>
2022-01-04 11:29:23 +0600

    add env

--- src/component/firebase/firbase.confing.js
+++ src/component/firebase/firbase.confing.js
@@ -1,4 +1,16 @@
 const firebaseConfig = {
+  apiKey: "AIzaSyALqNhtNYYOXv6h6kgwaOqlmIzH-jMFFAk",
+  authDomain: "devwear-web-app.firebaseapp.com",
+  projectId: "devwear-web-app",
+  storageBucket: "devwear-web-app.appspot.com",
+  messagingSenderId: "916679622655",
+  appId: "1:916679622655:web:f07e0182381c1f2615b1ca",
+};
+
+export default firebaseConfig;
+
+/* 
+const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
@@ -6,4 +18,4 @@ const firebaseConfig = {
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
 };
-export default firebaseConfig;
+export default firebaseConfig; */
